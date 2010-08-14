1e6a5eaf24fed0103f4f3853861331bbd6324572
andrews medina <andrewsmedina@gmail.com>
2010-08-14 20:51:09 -0300

    fix module name in import

--- wifimap/spots/tests/__init__.py
+++ wifimap/spots/tests/__init__.py
@@ -6,4 +6,4 @@ from spots.tests.test_search import *
 from spots.tests.test_list import *
 from spots.tests.test_detail import *
 from spots.tests.test_point_by_ip import *
-from spots.test.test_json_point import *
+from spots.tests.test_json_point import *
