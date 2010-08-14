05bad3730132b1664ba42818f41c9dab6b2ee3a8
andrews medina <andrewsmedina@gmail.com>
2010-08-14 16:27:48 -0300

    refactoring detail tests

--- wifimap/spots/tests/test_detail.py
+++ wifimap/spots/tests/test_detail.py
@@ -8,23 +8,20 @@ class DetailViewTest(TestCase):
     def setUp(self):
         self.access_point = AccessPoint.objects.create(name='point 1', description='Point 1 description', address='Rio de Janeiro, Brazil', lat=-22.9963233069, lng=-43.3637237549)
         self.url = reverse('spot_detail', args=[self.access_point.id])
+        self.response = self.client.get(self.url)
         
     def tearDown(self):
         AccessPoint.objects.all().delete()
         
     def test_view_exists(self):
-        response = self.client.get(self.url)    
-        assert 200 == response.status_code
+        assert 200 == self.response.status_code
         
     def test_name_in_spot_detail(self):
-        response = self.client.get(self.url)
-        assert 'point 1' in response.content
+        assert 'point 1' in self.response.content
         
     def test_address_in_spot_detail(self):
-        response = self.client.get(self.url)
-        assert 'Rio de Janeiro, Brazil' in response.content
+        assert 'Rio de Janeiro, Brazil' in self.response.content
         
     def test_description_in_spot_detail(self):
-        response = self.client.get(self.url)
-        assert 'Point 1 description' in response.content
+        assert 'Point 1 description' in self.response.content
 
