748c83050c0d6f8edf7fdd680a8578a0e1db77ea
Igor Sobreira <igor@igorsobreira.com>
2010-08-14 11:23:18 -0300

    spots models

--- wifimap/settings.py
+++ wifimap/settings.py
@@ -68,6 +68,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.messages',
-    # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
+    
+    'spots',
 )
--- wifimap/spots/models.py
+++ wifimap/spots/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils.translation import ugettext as _
 
-# Create your models here.
+class AccessPoint(models.Model):
+    name = models.CharField(_('name'), max_length=255)
+    address = models.CharField(_('address'), max_length=255)
+    description = models.TextField(_('description'), blank=True)
+    
+    lat = models.FloatField(editable=False)
+    lng = models.FloatField(editable=False)
+    
+    def __unicode__(self):
+        return self.name
