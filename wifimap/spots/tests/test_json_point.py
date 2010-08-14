0ec6f50525ffb15d2377e12c577983303a183b6a
andrews medina <andrewsmedina@gmail.com>
2010-08-14 20:49:59 -0300

    add tests for json point

---
+++ wifimap/spots/tests/test_json_point.py
@@ -0,0 +1,19 @@
+from django.test import TestCase
+from django.core.urlresolvers import reverse
+
+from spots.models import AccessPoint
+
+class JsonPointTest(TestCase):
+    
+    def setUp(self):
+        self.access_point = AccessPoint.objects.create(name='point 1', description='Point 1 description', address='Rio de Janeiro, Brazil', lat=-22.9963233069, lng=-43.3637237549)
+        self.url = reverse('spot_json_detail', args=[self.access_point.id])
+        self.response = self.client.get(self.url)
+        
+    def tearDown(self):
+        AccessPoint.objects.all().delete()
+        
+    def test_view_exists(self):
+        assert 200 == self.response.status_code
+        
+
