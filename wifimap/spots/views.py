99cc88d1ef2776b4060c55b395b3924d63550e15
Enrico <rico.bl@gmail.com>
2010-08-14 11:53:50 -0300

    Changed index view to use direct_to_template.

--- wifimap/spots/views.py
+++ wifimap/spots/views.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 
 from django.shortcuts import render_to_response
+from django.views.generic.simple import direct_to_template
 
 def index(request):
-    return render_to_response('index.html', {})
+    return direct_to_template(request, 'index.html', extra_context={})
 
