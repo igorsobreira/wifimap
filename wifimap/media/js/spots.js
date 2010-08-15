e2e497f206e92ffbc697c2bed4e532ced7e11db3
andrews medina <andrewsmedina@gmail.com>
2010-08-15 18:03:09 -0300

    fixed error with load points in chorme

--- wifimap/media/js/spots.js
+++ wifimap/media/js/spots.js
@@ -46,17 +46,20 @@ var SpotManager = {
                 navigator.geolocation.getCurrentPosition(function(position){
                     var initialLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
                     Map.map.setCenter(initialLocation);
+                    SpotManager.afterCentralize(data.points);
                 }, function() {
                     Map.map.setCenter(new google.maps.LatLng(data.center_point[1][0], data.center_point[1][1]));
+                    SpotManager.afterCentralize(data.points);
                 });
             } else {
                 Map.map.setCenter(new google.maps.LatLng(data.center_point[1][0], data.center_point[1][1]));                        
+                SpotManager.afterCentralize(data.points);
             }
         } else {
             Map.map.setCenter(new google.maps.LatLng(data.center_point[1][0], data.center_point[1][1]));
+            SpotManager.afterCentralize(data.points);
         }
         
-        SpotManager.afterCentralize(data.points);
     },
 
     listSpots: function() {
