f38d50d9d6f4f0485c56bd35086c98b3c990b8bc
andrews medina <andrewsmedina@gmail.com>
2010-08-15 23:23:24 -0300

    removing default center comment

--- wifimap/media/js/map.js
+++ wifimap/media/js/map.js
@@ -5,7 +5,6 @@ var Map = {
         this.container = $('#map');
         this.defaultOptions = {
             zoom: 15,
-            //center: new google.maps.LatLng(-22.9963233069, -43.3637237549),
             mapTypeId: google.maps.MapTypeId.HYBRID,
             scrollwheel: false
         };
