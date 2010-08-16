eec1e14e7a88efc118d260a0e9adf33319d1a572
andrews medina <andrewsmedina@gmail.com>
2010-08-15 23:20:31 -0300

    change map type to hybrid

--- wifimap/media/js/map.js
+++ wifimap/media/js/map.js
@@ -6,7 +6,7 @@ var Map = {
         this.defaultOptions = {
             zoom: 8,
             //center: new google.maps.LatLng(-22.9963233069, -43.3637237549),
-            mapTypeId: google.maps.MapTypeId.ROADMAP,
+            mapTypeId: google.maps.MapTypeId.HYBRID,
             scrollwheel: false
         };
         this.map = new google.maps.Map(this.container.get(0), this.defaultOptions);
