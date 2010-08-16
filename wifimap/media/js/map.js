d22e66abbfb6aaa1225d6fc5dd2e39f7e72e62e5
andrews medina <andrewsmedina@gmail.com>
2010-08-15 23:22:48 -0300

    changing default zoom again again

--- wifimap/media/js/map.js
+++ wifimap/media/js/map.js
@@ -4,7 +4,7 @@ var Map = {
     init: function() {
         this.container = $('#map');
         this.defaultOptions = {
-            zoom: 8,
+            zoom: 15,
             //center: new google.maps.LatLng(-22.9963233069, -43.3637237549),
             mapTypeId: google.maps.MapTypeId.HYBRID,
             scrollwheel: false
